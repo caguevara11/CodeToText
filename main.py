10471d5fe29590021b4deb087133759a87dbe965
caguevara11 <carlos.guevara@ucrso.info>
2024-06-21 02:13:15 -0600

    Added support for exporting GitHub repositories and handling local project paths.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+"""
+Main entry point for the CodeToText tool. This script handles the
+conversion of a git repository into a single well-formatted.txt or.md file.
+It supports both local and remote repositories, including GitHub URLs.
+"""
 import os
 import sys
 from exporter.main import ProjectExporter
 from exporter.github_handler import GitHubHandler
 
 def is_github_url(url: str) -> bool:
+    """
+    Checks if the provided URL is a GitHub URL.
+
+    Args:
+        url (str): The URL to check.
+
+    Returns:
+        bool: True if the URL is a GitHub URL, False otherwise.
+    """
     return url.startswith("https://github.com/") or url.startswith("git@github.com:")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Error: No project path or GitHub URL provided. Usage: main.py <project_path_or_github_url>")
+        print("Error: No project path or GitHub URL provided. \
+              Usage: main.py <project_path_or_github_url>")
         sys.exit(1)
 
     project_path_or_url = sys.argv[1]
@@ -27,7 +42,7 @@ if __name__ == "__main__":
     exporter = ProjectExporter()
     project_structure, project_contents = exporter.export(project_path)
 
-    with open(output_file_path, 'w') as file:
+    with open(output_file_path, 'w', encoding='utf-8') as file:
         file.write(project_structure)
         file.write('\n\n')
         file.write(project_contents)
